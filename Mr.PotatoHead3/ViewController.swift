a4cf8f9846a0677ccb0d7450d0cd7b81cc3dfd27
Jidde Koekoek <jiddekoekoek@macbook-pro-van-jidde.local>
2016-02-14 11:57:32 +0100

    Stack view + hide body parts, not fully working

--- Mr.PotatoHead3/ViewController.swift
+++ Mr.PotatoHead3/ViewController.swift
@@ -9,10 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var imageViewArray: [UIImageView] = []
+    var buttonArray: [UISwitch] = []
+    
+    @IBOutlet weak var body: UIImageView!
+    @IBOutlet weak var arms: UIImageView!
+    @IBOutlet weak var ears: UIImageView!
+    @IBOutlet weak var eyebrows: UIImageView!
+    @IBOutlet weak var eyes: UIImageView!
+    @IBOutlet weak var glasses: UIImageView!
+    @IBOutlet weak var hat: UIImageView!
+    @IBOutlet weak var mouth: UIImageView!
+    @IBOutlet weak var mustache: UIImageView!
+    @IBOutlet weak var nose: UIImageView!
+    @IBOutlet weak var shoes: UIImageView!
+    
+    @IBOutlet weak var button1: UISwitch!
+    @IBOutlet weak var button2: UISwitch!
+    @IBOutlet weak var button3: UISwitch!
+    @IBOutlet weak var button4: UISwitch!
+    @IBOutlet weak var button5: UISwitch!
+    @IBOutlet weak var button6: UISwitch!
+    @IBOutlet weak var button7: UISwitch!
+    @IBOutlet weak var button8: UISwitch!
+    @IBOutlet weak var button9: UISwitch!
+    @IBOutlet weak var button10: UISwitch!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imageViewArray += [arms, ears, eyebrows, eyes, glasses, hat, mouth, mustache, nose, shoes]
+        
+        buttonArray += [button1, button2, button3, button4, button5, button6, button7, button8, button9, button10]
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +50,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func showBodyParts(sender: AnyObject) {
+        for (index, button) in buttonArray.enumerate(){
+            if button != button.enabled{
+                imageViewArray[index].hidden = true
+            }
+            else{
+                imageViewArray[index].hidden = false
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
 
